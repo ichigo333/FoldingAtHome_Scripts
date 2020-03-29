fad104f3cf10b67f023e9b35643652e9220141ec
Alex <alex333sh@gmail.com>
2020-03-29 16:36:40 -0400

    adding main, removing random global vars

--- parse_wus.py
+++ parse_wus.py
@@ -2,7 +2,7 @@ import json, time, requests, sys, os
 from config import Config
 
 
-def get_value(index, name):
+def get_value(lineSplit, index, name):
     try:
         if name in lineSplit[index]:
             return lineSplit[index].split(':')[1]
@@ -23,7 +23,6 @@ def get_apiInfo(project, run, clone, gen, user):
     except:
         print("ERROR: cannot get info from API")
 
-
 def read_config():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     configFile = open(f"{dir_path}/config.json", "r")
@@ -37,41 +36,41 @@ def read_config():
     return Config(inputPath, outputPath, user)
 
 
+def main():
+    config = read_config()
 
-config = read_config()
+    file = open(config.inputPath, "r")
+    lineList = file.readlines()
 
-file = open(config.inputPath, "r")
-lineList = file.readlines()
+    project = ""
 
-project = ""
+    for line in lineList:
+        lineSplit = line.split(" ")
 
-for line in lineList:
-    lineSplit = line.split(" ")
+        project = get_value(lineSplit, 6,'project')
+        run = get_value(lineSplit, 7,'run')
+        clone = get_value(lineSplit, 8,'clone')
+        gen = get_value(lineSplit,9,'gen')
+        unit = get_value(lineSplit, 11,'unit').rstrip()
 
-    project = get_value(6,'project')
-    run = get_value(7,'run')
-    clone = get_value(8,'clone')
-    gen = get_value(9,'gen')
-    unit = get_value(11,'unit').rstrip()
-
-    wu = get_apiInfo(project, run, clone, gen, config.user)
-    if wu != "":
-        code = wu.get('code')
-        credit = wu.get('credit')
-        credit_time = wu.get('credit_time')
-        days = wu.get('days')
-        cpuid = wu.get('cpuid')
-    else:
-        code = ""
-        credit = ""
-        credit_time = ""
-        days = ""
-        cpuid = ""
-
-
-    print(f"{project},{run},{clone},{gen},{unit},{code},{credit},{credit_time},{days},{cpuid}")
-
-##print("")
+        wu = get_apiInfo(project, run, clone, gen, config.user)
+        if wu != "":
+            code = wu.get('code')
+            credit = wu.get('credit')
+            credit_time = wu.get('credit_time')
+            days = wu.get('days')
+            cpuid = wu.get('cpuid')
+        else:
+            code = ""
+            credit = ""
+            credit_time = ""
+            days = ""
+            cpuid = ""
 
 
-file.close()
+        print(f"{project},{run},{clone},{gen},{unit},{code},{credit},{credit_time},{days},{cpuid}")
+
+    file.close()
+
+if __name__ == "__main__":
+    main()
